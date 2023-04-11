2185191705e849ecefbda3c629ea5726f07ae6ea
Sebastien Boudreau Laprise <boulap.seb@gmail.com>
2023-04-11 08:46:37 -0700

    Project Commit

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,6 @@
+const path = require("path");
 const { MongoClient } = require("mongodb");
-const delay = require("delay");
+const { v4: uuidv4 } = require("uuid");
 require("dotenv").config({ path: "../.env" });
 const { MONGO_URI } = process.env;
 
@@ -8,24 +9,91 @@ const options = {
   useUnifiedTopology: true,
 };
 
-const postForm = async (req, res) => {
-  const { email } = req.body;
-  await delay(Math.random() * 3000);
-  //   if (!email.includes("@")) {
-  //     return res.status(400).json({
-  //       status: "error",
-  //       error: "missing-data",
-  //     });
-  //   }
+// Forms modification
+const getForms = async (request, response) => {
+  try {
+    const client = await MongoClient.connect(MONGO_URI, {
+      useUnifiedTopology: true,
+    });
+    const dbName = client.db("Cosmic_NRG");
 
+    const data = await dbName.collection("forms").find().toArray();
+
+    client.close();
+    return response.status(200).json({ status: 200, data: data });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ status: 500, message: "Server Error, " + error.message });
+  }
+};
+const getSingleForm = async (request, response) => {
+  try {
+    const { _id } = request.params;
+    const client = await MongoClient.connect(MONGO_URI, {
+      useUnifiedTopology: true,
+    });
+    const dbName = client.db("Cosmic_NRG");
+    const query = { _id: _id };
+    const data = await dbName.collection("forms").findOne(query);
+    console.log(data);
+    client.close();
+    return response.status(200).json({ status: 200, data: data });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ status: 500, message: "Server Error, " + error.message });
+  }
+};
+
+const postNewForm = async (request, response) => {
+  try {
+    const { type, formData } = request.body;
+
+    console.log("request.body", request.body);
+
+    const client = await MongoClient.connect(MONGO_URI);
+    const dbName = client.db("Cosmic_NRG");
+
+    // Create a Date object with the current date and time
+    const now = new Date();
+    const timestamp = now.getTime();
+
+    const addForm = {
+      formData,
+      type,
+      status: "pending",
+      comment: [],
+      _id: uuidv4(),
+      date: timestamp,
+    };
+
+    // create form
+    const newForm = await dbName.collection("forms").insertOne(addForm);
+    client.close();
+    return response.status(200).json({
+      status: 200,
+      message: "success",
+    });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ status: 500, message: "Server Error, " + error.message });
+  }
+};
+
+const deleteForm = async (req, res) => {
+  const id = req.params.forms;
   const client = new MongoClient(MONGO_URI, options);
   const db = client.db("Cosmic_NRG");
-
   try {
     await client.connect();
-    await db.collection("forms").insertOne({
-      ...req.body,
-    });
+    const form = await db.collection("forms").findOne({ id });
+    if (!form) {
+      return res.status(404).json({ status: 404, message: "Form not found" });
+    }
+    await db.collection("forms").deleteOne({ id });
+    client.close();
     return res.status(200).json({
       status: 200,
       success: true,
@@ -36,4 +104,146 @@ const postForm = async (req, res) => {
   }
 };
 
-module.exports = { postForm };
+const markAsResolved = async (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+  try {
+    const client = new MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("Cosmic_NRG");
+    const form = await db.collection("forms").findOne({ _id: id });
+    console.log(form);
+    if (!form) {
+      client.close();
+      return response
+        .status(404)
+        .json({ status: 404, message: "Reservation not found" });
+    }
+    await db
+      .collection("forms")
+      .updateOne({ _id: id }, { $set: { status: "resolved" } });
+
+    client.close();
+    return response.status(200).json({
+      status: 200,
+      message: "successfully modified ",
+    });
+  } catch (error) {
+    return response
+
+      .status(500)
+      .json({ status: 500, message: "Server Error: " + error.message });
+  }
+};
+
+const updateForm = async (req, res) => {
+  const { updatedfirstName, updatedLastName, updatedEmail, updatedSeat } =
+    req.body;
+  const _id = req.params.reservation;
+  const client = new MongoClient(MONGO_URI, options);
+  const db = client.db("slingshot");
+  try {
+    await client.connect();
+    const reservation = await db.collection("reservations").findOne({ _id });
+
+    if (!reservation) {
+      client.close();
+      return res
+        .status(404)
+        .json({ status: 404, message: "Reservation not found" });
+    }
+    const newFirstName = updatedfirstName
+      ? updatedfirstName
+      : reservation.firstName;
+    const newLastName = updatedLastName
+      ? updatedLastName
+      : reservation.lastName;
+    const newEmail = updatedEmail ? updatedEmail : reservation.email;
+    const newSeat = updatedSeat ? updatedSeat : reservation.seat;
+
+    console.log(updatedSeat);
+    if (updatedSeat) {
+      const updatePastSeat = await db.collection("flights").updateOne(
+        {
+          flight: reservation.flight,
+          "seats.id": reservation.seat,
+        },
+
+        { $set: { "seats.$.isAvailable": true } }
+      );
+      const updateNewSeat = await db.collection("flights").updateOne(
+        {
+          flight: reservation.flight,
+          "seats.id": updatedSeat,
+        },
+
+        { $set: { "seats.$.isAvailable": false } }
+      );
+    }
+    await db.collection("reservations").updateOne(
+      { _id: reservation._id },
+      {
+        $set: {
+          firstName: newFirstName,
+          lastName: newLastName,
+          email: newEmail,
+          seat: newSeat,
+        },
+      }
+    );
+
+    client.close();
+    return res.status(200).json({
+      status: 200,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+// Admin users
+const handleUser = async (req, res) => {
+  const user = req.body;
+
+  const currentUser = new MongoClient(MONGO_URI, options);
+  const db = client.db("CosmicNRG");
+  try {
+    await client.connect();
+    const userEntry = await db
+      .collection("users")
+      .findOne({ email: user.email });
+
+    if (!userEntry) {
+      await db.collection("users").insertOne(user);
+      res.status(200).json({ status: 200, message: "User added to database" });
+    } else {
+      res.status(400).json({ status: 400, message: "User exists" });
+    }
+  } catch (error) {
+    console.log(error.stack);
+    res.status(500).json({ status: 500, error: error.stack });
+  }
+};
+// Post Comment
+const createComment = (status, { isRetweet }) => {
+  const newTweetId = Math.random() * 10 ** 18;
+  const timestamp = new Date().toISOString();
+
+  let sharedTweetBasics = {
+    _id: newTweetId,
+    authorHandle: CURRENT_USER_HANDLE,
+    timestamp,
+    sortedTimestamp: timestamp,
+  };
+};
+
+module.exports = {
+  postNewForm,
+  deleteForm,
+  markAsResolved,
+  updateForm,
+  getForms,
+  handleUser,
+  getSingleForm,
+};
--- server/server.js
+++ server/server.js
@@ -1,21 +1,64 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { join } = require("path");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const cloudinary = require("cloudinary").v2;
+const MONGO_URI = process.env.MONGO_URI;
 const PORT = 8000;
 const app = express();
-const { postForm } = require("./handlers");
+const {
+  postNewForm,
+  deleteForm,
+  markAsResolved,
+  updateForm,
+  getForms,
+  handleUser,
+  getSingleForm,
+} = require("./handlers");
 app.use(express.json());
 
+app.use(morgan("dev"));
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// app.use(express.static(join(__dirname, "build")));
+//works
+app.get("/getForms", getForms);
+app.get("/getForm/:_id", getSingleForm);
+app.delete("/deleteForm/:id", deleteForm);
+//dont work
+app.post("/postForm", postNewForm);
+app.patch("/resolved/:id", markAsResolved);
+app.patch("/modifyForm/:id", updateForm);
+app.post("/addUser", handleUser);
+// app.get("/api/account", getAccounts);
+// app.get("/api/account/:id", getAccountById);
+// app.post("/api/signin", loginAccount);
+// app.patch("/api/friends", handleFriends);
+
+// Serve static assets from the /public folder
+// app.use(express.static(join(__dirname, "public")));
+
+// Endpoint to serve the configuration file
+// app.get("/auth_config.json", (req, res) => {
+//   res.sendFile(join(__dirname, "auth_config.json"));
+// });
+
+// Serve the index page for all other requests
+// app.get("/*", (_, res) => {
+//   res.sendFile(join(__dirname, "index.html"));
+// });
 app.get("*", (req, res) => {
   res.status(404).json({
     status: 404,
     message: "This is obviously not what you are looking for.",
   });
 });
-app.post("/contact", postForm);
 
-// app.get("/api/account", getAccounts);
-// app.get("/api/account/:id", getAccountById);
-// app.post("/api/signin", loginAccount);
-// app.patch("/api/friends", handleFriends);
-// app.delete("/api/account/:id", deleteAccount);
-// Node spins up our server and sets it to listen on port 8000.
 app.listen(PORT, () => console.log(`Listening on port 8000`));
